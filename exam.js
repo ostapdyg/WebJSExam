f8167f617cb11c6010500b5ccc3ae145e8a7039b
ostapdyg <dyhdalovych@ucu.edu.ua>
2020-04-09 11:34:03 +0300

    Upd

--- exam.js
+++ exam.js
@@ -1,21 +1,19 @@
-function tune(arr){
+function tune(arr) {
     correct = [329.63, 246.94, 196, 146.83, 110, 82.41];
     res = []
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === 0){
-            res[i] = " - "
-        }else{
-            percent = (arr[i]-correct[i])/correct[i];
-            res[i] = [">>•",">•", "OK", "•<", "•<<"][(percent>-0.03)+(arr[i]-correct[i]>=0)+(arr[i]-correct[i]>0)+(percent>0.03)];
-            console.log(percent);
-
-        }
+        percent = (arr[i] - correct[i]) / correct[i];
+        res[i] = [" - ", ">>•", ">•", "OK", "•<", "•<<"][(arr[i] != 0) * (1 + 
+                                                         (percent > -0.03) + 
+                                                         (arr[i] - correct[i] >= 0) + 
+                                                         (arr[i] - correct[i] > 0) + 
+                                                         (percent > 0.03))];
     }
     return res;
 }
 
-console.log(tune([0, 246.94, 0, 0, 0, 80])) //➞ [" - ", "OK", " - ", " - ", " - ", ">>•"]
+// console.log(tune([0, 246.94, 0, 0, 0, 80])) //➞ [" - ", "OK", " - ", " - ", " - ", ">>•"]
 
-console.log(tune([329, 246, 195, 146, 111, 82])) //➞ ["OK", "OK", ">•", ">•", "•<", "OK" ]
+// console.log(tune([329, 246, 195, 146, 111, 82])) //➞ ["OK", "OK", ">•", ">•", "•<", "OK" ]
 
-console.log(tune([329.63, 246.94, 196, 146.83, 110, 82.41])) //➞ ["OK", "OK", "OK", "OK", "OK", "OK"]
+// console.log(tune([329.63, 246.94, 196, 146.83, 110, 82.41])) //➞ ["OK", "OK", "OK", "OK", "OK", "OK"]
